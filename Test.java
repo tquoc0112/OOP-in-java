ec3b4eaed5a7b4657b576ba7e4db132ffa4f7422
Pham Thai Quoc <phamthaiquoc0112@gmail.com>
2023-02-16 12:06:47 +0700

    quiz 1 oop with java 16/2/2023

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test{
+    public static void main(String[] args){
+        Plants p = new Plants();
+        p.setX(x=5);
+        p.setY(y=1);
+        p.SayHello();
+    }
+}
+
---
+++ Zombies.java
@@ -0,0 +1,12 @@
+public class Zombies{
+    private byte speed;
+    private int id=0;
+    private int x;
+    private int y;
+    public int getX(){
+        return x;
+    }
+    public void setX(int x){
+        this.x= x;
+    }
+}
