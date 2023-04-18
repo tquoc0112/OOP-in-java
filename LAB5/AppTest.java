55a614d9cf3ec2f5417aeac0a774074456d8bace
tquoc0112 <124855237+tquoc0112@users.noreply.github.com>
2023-04-18 14:16:19 +0700

    Update AppTest.java

--- LAB5/AppTest.java
+++ LAB5/AppTest.java
@@ -7,20 +7,20 @@ import java.util.Locale;
 public class AppTest {
     public static void main(String []args) throws ParseException {
         //initialize date formatter
-        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
 
         //create project object
         Project project = new Project();
         //initialize project object with some values
         project.setProjectId("1");
-        project.setStartDate( sdf.parse("06/24/2017"));
-        project.setEndDate(sdf.parse("06/30/2017"));
+        project.setStartDate( sdf.parse("17/03/2023"));
+        project.setEndDate(sdf.parse("01/12/2023"));
 
         ArrayList<Employee> employeeArrayList = new ArrayList<>();
-        employeeArrayList.add(new Employee("1","PhamThaiQuoc",200,2,3));
-        employeeArrayList.add(new Employee("2","Hasbulla",300,3,3));
-        employeeArrayList.add(new Employee("3","Mario",350,2,1));
-        employeeArrayList.add(new Employee("4","Sonic",350,1,1));
+        employeeArrayList.add(new Employee("1","PhamThaiQuoc",500,4,4));
+        employeeArrayList.add(new Employee("2","Hasbulla",460,3,3));
+        employeeArrayList.add(new Employee("3","Mario",490,2,2));
+        employeeArrayList.add(new Employee("4","Sonic",450,1,1));
 
         project.setListOfEmployees(employeeArrayList);
 
